2322d0a556e34cac649c90975283a15a32c76f05
Christian Cuevas <ccpotential@gmail.com>
2018-01-03 18:27:04 -0800

    update projects
    Clark was here :V

---
+++ src/preview/WS-potential/js/core/config.js
@@ -0,0 +1,93 @@
+var WORKSPACE = {
+  "version": "0.0.0",
+  "type": "work space",
+  "themes": {
+
+  },
+  "projects": {
+    "image": { // Image manipulation
+
+    },
+    "vector": { // Vector manipulation
+
+    },
+    "frame": { // Frame by frame animation
+
+    },
+    "skeleton": { // Skeleton animation
+
+    },
+    "video": { // Video manipulation
+
+    },
+    "audio": { // Audio manipulation
+
+    }
+  }
+};
+
+/**
+ * Saves the current work space to the local storage of the browser.
+ * @return {undefined} Returns nothing.
+ */
+function workspaceSave() {
+
+}
+
+/**
+ * Loads and sets up a given work space object.
+ * @param {Object/String} [work_space] A work space object, or a String representing a work space object.
+ * @return {undefined} Returns nothing.
+ */
+function workspaceLoad() {
+
+}
+
+/**
+ * Exports the current work space as a 2DP file.
+ * @return {undefined} Returns nothing.
+ */
+function workspaceExport() {
+
+}
+
+/**
+ * Saves a given project.
+ * @param {Object/String} [project] A project object, or a string representing a project.
+ * @param {Boolean} [current] Whether or not the current project is being exported.
+ * @return {undefined} Returns nothing.
+ */
+function projectSave(project, current) {
+
+}
+
+/**
+ * Loads a given project object.
+ * @param {Object/String} [project] A project object, or a string representing a project.
+ * @param {Boolean} [current] Whether or not the current project is being exported.
+ * @return {undefined} Returns nothing.
+ */
+function projectLoad(project, current) {
+
+}
+
+/**
+ * Exports a given project object as a 2DP file.
+ * @param {Object/String} [project] A project object, or a string representing a project.
+ * @param {Boolean} [current] Whether or not the current project is being exported.
+ * @return {undefined} Returns nothing.
+ */
+function workspaceExport(project, current) {
+
+}
+
+/**
+ * Exports a given project object as a given file type.
+ * @param {Object/String} [project] A project object, or a string representing a project.
+ * @param {String} [file_type] File type to export as.
+ * @param {Boolean} [current] Whether or not the current project is being exported.
+ * @return {undefined} Returns nothing.
+ */
+function workspaceExportAs(project, file_type, current) {
+
+}
