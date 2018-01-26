b90c42d62fdb58849a3aaa0a078508eff4aeac4a
Christian Cuevas <ccpotential@gmail.com>
2018-01-25 21:47:07 -0800

    redoing some stuff
    Clark was here :V

--- src/preview/Interactive List/1.0.0/js/index.js
+++ src/preview/Interactive List/1.0.0/js/index.js
@@ -1,82 +1,87 @@
-var style = 0,
-  objects = [],
-  selectedObjects = [];
+var style = 0;
+var tree = {
+  active: [], // Branches that are active
+  root: [], // Branches found at the root of the tree
+  branches: {}, // All the branches attached to the tree
+  branches_total: 0 // Total number of branches in the tree's lifetime
+};
 
 window.onload = function() {
-  // Create root object
-  objCreate("Project Objects", "folder");
-
-  // Testing objects
-  objCreate("Head", "sprite", 0);
-  objCreate("Body", "sprite", 0);
-  objCreate("Hands", "sprite", 0);
-  objCreate("Legs", "sprite", 0);
-  objCreate("Feet", "sprite", 0);
+  // Testing branches
+  objCreate("Head", "sprite");
+  objCreate("Body", "sprite");
+  objCreate("Hands", "sprite");
+  objCreate("Legs", "sprite");
+  objCreate("Feet", "sprite");
 };
 
 
 /**
- * Function to handle the start of a object's dragging.
+ * Function to handle the action "drag start" on a branch.
  * @param {Event} [e] Event to be handled.
  * @return {undefined} Returns nothing.
  */
-function objManagingDragStart(e) {
-  // Check if it's invalid
+function branchDragStart(e) {
+  // Check if it's a invalid target; meaning target isn't a disabled branch
   if (e.target.dataset.id == 0 || e.target.dataset.id == "error") {
     e.preventDefault();
     return;
   }
 
+  // Setup data for data transfer
   e.dataTransfer.setData("obj", e.target.dataset.id);
   e.dataTransfer.setData("type", "object");
 }
 
 
 /**
- * Function to handle dragging enter of an object within object managing.
+ * Function to handle the action "drag enter" on a branch.
  * @param {Event} [e] Event to be handled.
  * @return {undefined} Returns nothing.
  */
-function objManagingDragEnter(e) {
-  // Check if it's invalid
+function branchDragEnter(e) {
+  // Check if it's not a invalid target; meaning target isn't itself or a disabled branch
   if ((e.target.dataset.id || e.target.parentNode.dataset.id) === e.dataTransfer.getData("obj") || (e.target.dataset.id || e.target.parentNode.dataset.id) == 0 || (e.target.dataset.id || e.target.parentNode.dataset.id) == "error") {
     e.preventDefault();
     return;
   }
 
+  // Add CSS "hover" class to target's parent DOM
   e.target.parentNode.classList.add("hover-" + e.target.classList[0]);
 }
 
 
 /**
- * Function to handle dragging over of an object within object managing.
+ * Function to handle the action "drag over" on a branch.
  * @param {Event} [e] Event to be handled.
  * @return {undefined} Returns nothing.
  */
-function objManagingDragOver(e) {
+function branchDragOver(e) {
   e.preventDefault();
 }
 
 
 /**
- * Function to handle dragging leave of an object within object managing.
+ * Function to handle the action "drag exit" on a branch.
  * @param {Event} [e] Event to be handled.
  * @return {undefined} Returns nothing.
  */
-function objManagingDragLeave(e) {
+function branchDragExit(e) {
+  // Remove CSS "hover" class to target's parent DOM
   e.target.parentNode.classList.remove("hover-" + e.target.classList[0]);
 }
 
 
 /**
- * Function to handle dropping of an object within object managing.
+ * Function to handle the action "drag drop" on a branch.
  * @param {Event} [e] Event to be handled.
  * @return {undefined} Returns nothing.
  */
-function objManagingDragDrop(e) {
+function branchDragDrop(e) {
+  // Remove CSS "hover" class to target's parent DOM
   e.target.parentNode.classList.remove("hover-" + e.target.classList[0]);
 
-  // Check if it's invalid
+  // Check if it's invalid target; meaning target isn't itself or a disabled branch
   if ((e.target.dataset.id || e.target.parentNode.dataset.id) == 0 || (e.target.dataset.id || e.target.parentNode.dataset.id) == "error") {
     e.preventDefault();
     return;
@@ -92,56 +97,65 @@ function objManagingDragDrop(e) {
   }
 
   if (e.target.classList.contains("above")) {
-    objMove(obj, target, "above");
+    branchMove(tree.branches[obj], tree.branches[target], "above");
   } else if (e.target.classList.contains("content")) {
-    objMove(obj, target, "into");
+    branchMove(tree.branches[obj], tree.branches[target], "into");
   } else if (e.target.classList.contains("bottom")) {
-    objMove(obj, target, "below");
+    branchMove(tree.branches[obj], tree.branches[target], "below");
   }
 }
 
 
 /**
- * AMove a object, within objects, to another position, within objects; and update all the object managers;
- * @param {Integer} [obj] Obj to move.
- * @param {Integer} [target] Target to move obj to.
- * @param {String} [operation] Where should obj be placed relative to target; Options: above, into, below.
+ * Move a branch, within the tree, to another position; and update all the object managers.
+ * @param {Branch} [branch] Branch to be moved.
+ * @param {Branch} [target_branch] "Branch" will be moved relative to "target branch".
+ * @param {String} [operation] Where "branch" should be set relative to "target branch"; Options: above, into, below.
  * @return {undefined} Returns nothing.
  */
-function objMove(obj, target, operation) {
-  // Setup variables
-  var index;
-  obj = objects[obj];
-  target = objects[target];
+function branchMove(branch = undefined, target_branch = undefined, operation = "into") {
+  // If "branch" or "target branch" is not given return
+  if (branch == undefined || target_branch == undefined) {
+    return;
+  }
 
-  // Remove links between object and root
-  objects[obj.root].branches.splice(objects[obj.root].branches.indexOf(obj.id), 1);
+  // If branch has a root branch do the following
+  if (branch.root !== undefined) {
+    // From branch's root remove itself
+    tree.branches[branch.root].branches.splice(tree.branches[branch.root].branches.indexOf(branch.id), 1);
+  } else {
+    // From tree's root remove itself
+    tree.root.splice(tree.root.indexOf(branch.id), 1);
+  }
 
-  // If object is already in target's root then remove it
-  index = objects[target.root].branches.indexOf(obj.id);
-  while (index !== -1) {
-    objects[target.root].branches.splice(index, 1);
+  if (target_branch.root !== undefined) {
+    // If branch is in branch target's root then remove itself
+    let index = tree.branches[target_branch.root].branches.indexOf(branch.id);
+    while (index !== -1) {
+      tree.branches[target_branch.root].branches.splice(index, 1);
 
-    index = objects[target.root].branches.indexOf(obj.id);
+      index = tree.branches[target_branch.root].branches.indexOf(branch.id);
+    }
   }
 
   if (operation == "above") {
-    // Move obj below the target in root
-    objects[target.root].branches.splice(objects[target.root].branches.indexOf(target.id), 0, obj.id);
+    // Move branch below the targeted branch in root
+    tree.branches[target_branch.root].branches.splice(tree.branches[target_branch.root].branches.indexOf(target_branch.id), 0, branch.id);
 
-    // Setup root for obj
-    objects[obj.id].root = target.root;
+    // Setup root for branch
+    tree.branches[branch.id].root = target_branch.root;
   } else if (operation == "into") {
-    objects[target.id].branches.push(obj.id);
+    // Move branch into the targeted branch
+    tree.branches[target_branch.id].branches.push(branch.id);
 
-    // Setup root for obj
-    objects[obj.id].root = target.id;
+    // Setup root for branch
+    tree.branches[branch.id].root = target_branch.id;
   } else if (operation == "below") {
-    // Move obj below the target in root
-    objects[target.root].branches.splice(objects[target.root].branches.indexOf(target.id) + 1, 0, obj.id);
+    // Move branch below the targeted branch in root
+    tree.branches[target_branch.root].branches.splice(tree.branches[target_branch.root].branches.indexOf(target_branch.id) + 1, 0, branch.id);
 
-    // Setup root for obj
-    objects[obj.id].root = target.root;
+    // Setup root for branch
+    tree.branches[branch.id].root = target_branch.root;
   }
 
   updateObjectManagers();
@@ -149,50 +163,54 @@ function objMove(obj, target, operation) {
 
 
 /**
- * Search for a specific object within a object.
- * @param {String} [name] Name of object searching for.
- * @param {String} [type] Type of object to search for.
- * @param {Object} [obj] Object to search through.
- * @return {Object} Returns a object filled with valid objects; empty object, neither this object or it's branches-objects are valid.
+ * Search for a specific branch within another branch.
+ * @param {String} [search_term] Search term used to identify branch.
+ * @param {String} [search_type] Type of branch we're searching for.
+ * @param {Object} [branch] Branch to search through.
+ * @return {Branch} Returns a branch filled with valid branches; else, empty object if neither this object or it's branches are valid.
  */
-function objSearch(name, type, obj) {
-  // Setup variables
-  name = name || "<any>";
-  type = type || "any";
-
+function branchSearch(search_term = "<any>", search_type = "any", branch) {
+  // If branch is not given return
+  if (branch == undefined) {
+    return;
+  }
   var result;
 
   // Check if this object is valid
-  if ((name === "<any>" || (obj.name).toLowerCase().indexOf(name.toLowerCase()) !== -1) && obj.name !== objects[0].name) {
-    if (type === "any" || type === obj.type) {
-      result = obj;
+  // First: check if substring in name has the search term
+  if (search_term === "<any>" || (branch.name).toLowerCase().indexOf(search_term.toLowerCase()) !== -1) {
+    // Second: check if the branch type matches the search type
+    if (search_type === "any" || search_type === branch.type) {
+      result = branch;
     } else {
       result = {
         "id": "error",
         "name": "Not Found"
-      };
+      }
     }
   }
-  // Check if this object has valid branches objects
+  // Check if this object has any valid branches
   else {
-    result = Object.assign({}, obj);
-    result.branches = [];
+    result = Object.assign({
+      branches: []
+    }, branch);
+    // For branch within this branch do a search
+    for (let branches of branch.branches) {
+      let subResult = branchSearch(search_term, search_type, objects[branches]);
 
-    var branches, subResult;
-    for (branches in obj.branches) {
-      subResult = objSearch(name, type, objects[obj.branches[branches]]);
-
-      // If a valid object is found push it to result
+      // If a valid branch is returned append to branches
       if (subResult.id !== "error") {
         result.branches.push(subResult);
       }
     }
 
+    // If no valid branches are found then return a error branch
     if (result.branches.length === 0) {
       result = {
         "id": "error",
         "name": "Not Found"
       };
+      // Else set real refrences to true
     } else {
       // result.open = true;
       result.realRefrences = true;
@@ -200,31 +218,6 @@ function objSearch(name, type, obj) {
   }
 
   return result;
-}
-
-
-/**
- * Create HTML, meant for object managing, out of a given object's data.
- * @param {Object} [obj] Name of the file.
- * @return {String} String representing object's data in HTML.
- */
-function objToHTML(obj) {
-  // Setup variables
-  var inner = "";
-
-  if (obj.open == true) {
-    for (var branches in obj.branches) {
-      if (obj.realRefrences) {
-        inner += objToHTML(obj.branches[branches]);
-      } else {
-        inner += objToHTML(objects[obj.branches[branches]]);
-      }
-    }
-  } else {
-    inner = null;
-  }
-
-  return '<li class="item" draggable="true" ondragstart="objManagingDragStart(event);" ondragenter="objManagingDragEnter(event);" ondragover="objManagingDragOver(event);" ondragleave="objManagingDragLeave(event);" ondrop="objManagingDragDrop(event);" data-id="' + obj.id + '"> <div class="above"></div> <div class="content"> <span onclick="objToggle(this.parentNode.parentNode.dataset.id)" class="toggle">' + (inner == "" ? "&#9900" : "+" /** (objects[obj.id].open ? "-" : "+") **/ ) + '</span>' + obj.name + '<span onclick="objDelete(this.parentNode.parentNode.dataset.id);" class="remove">x</span></div> <ul class="item-menu">' + (inner == null ? "" : inner) + '</ul> <div class="bottom"></div> </li>';
 }
 
 
@@ -237,7 +230,7 @@ function objToHTML(obj) {
 function objCreate(name, type, root) {
   // Setup variables
   type = type || "sprite";
-  name = name || type + " " + objects.length;
+  name = name || type + " " + tree.branches.length;
 
   // Object's Structure:
   // {String} [object's name] : {
@@ -250,44 +243,51 @@ function objCreate(name, type, root) {
   //  "realRefrences": {Boolean} [wether refrences to objects are objects themselves or ids]
   //
   //  }
-  var key = objects.push({
-    id: objects.length,
+  tree.branches[tree.branches_total] = {
+    id: tree.branches_total,
     name: name,
     type: type,
     root: root,
     branches: [],
     realRefrences: false,
     open: true
-  }) - 1;
+  };
+  tree.branches_total += 1;
 
-  // Add as branches to root
-  if (root !== undefined) {
-    objects[root].branches.push(key);
+  if (root === undefined) {
+    // Add as branches to tree root
+    tree.root.push(tree.branches_total - 1);
+  } else {
+    // Add as branch to branches root
+    tree.branches[root].branches.push(tree.branches_total - 1);
   }
 
   updateObjectManagers();
 
-  return key;
+  return tree.branches_total - 1;
 }
 
 
 /**
- * Delete a object.
- * @param {Integer} [obj_id] ID of object to delete.
+ * Delete a branch and all it's branches.
+ * @param {Integer} [branch_id] ID of branch to delete.
  * @return {undefined} Returns nothing.
  */
-function objDelete(obj_id) {
-  delete objects[obj_id];
+function branchDelete(branch_id) {
+  for (let branch of tree.branches[obj_id].branches) {
+    delete tree.branches[branch.id];
+  }
+  delete tree.branches[branch_id];
   updateObjectManagers();
 }
 
 /**
- * Toggle a object.
- * @param {Integer} [obj_id] ID of object to toggle.
+ * Toggle a branch.
+ * @param {Integer} [branch_id] ID of branch to toggle.
  * @return {undefined} Returns nothing.
  */
-function objToggle(obj_id) {
-  objects[obj_id].open = !objects[obj_id].open;
+function branchToggle(branch_id) {
+  tree.branches[branch_id].open = !tree.branches[branch_id].open;
   updateObjectManagers();
 }
 
@@ -299,9 +299,37 @@ function objToggle(obj_id) {
 function updateObjectManagers() {
   let object_managers = document.querySelectorAll("#object_manager");
   for (let object_manager of object_managers) {
-    let objs = objSearch(object_manager.obj_search.value, object_manager.obj_filter.value, objects[0]);
-    object_manager.querySelector("[name=obj_view]").innerHTML = objToHTML(objs);
+    var branches = "";
+    for (let branch of tree.root) {
+      branches += branchToHTML(branchSearch(object_manager.obj_search.value, object_manager.obj_filter.value, tree.branches[branch]));
+    }
+    object_manager.querySelector("[name=obj_view]").innerHTML = branches;
   }
+}
+
+
+/**
+ * Create HTML, meant for object managing, out of a given object's data.
+ * @param {Object} [branch] Name of the file.
+ * @return {String} String representing object's data in HTML.
+ */
+function branchToHTML(branch) {
+  // Setup variables
+  var inner = "";
+
+  if (branch.open == true) {
+    for (let sub_branch of branch.branches) {
+      if (sub_branch.realRefrences) {
+        inner += branchToHTML(branch.branches[sub_branch]);
+      } else {
+        inner += branchToHTML(tree.branches[sub_branch]);
+      }
+    }
+  } else {
+    inner = null;
+  }
+
+  return '<li class="item" draggable="true" ondragstart="branchDragStart(event);" ondragenter="branchDragEnter(event);" ondragover="branchDragOver(event);" ondragleave="branchDragExit(event);" ondrop="branchDragDrop(event);" data-id="' + branch.id + '"> <div class="above"></div> <div class="content"> <span onclick="branchToggle(this.parentNode.parentNode.dataset.id)" class="toggle">' + (inner == "" ? "&#9900" : "+" /** (tree.branches[branch.id].open ? "-" : "+") **/ ) + '</span>' + branch.name + '<span onclick="branchDelete(this.parentNode.parentNode.dataset.id);" class="remove">x</span></div> <ul class="item-menu">' + (inner == null ? "" : inner) + '</ul> <div class="bottom"></div> </li>';
 }
 
 
