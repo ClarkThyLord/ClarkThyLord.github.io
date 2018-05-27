7279a859c29862b1c47b8d34e7dc0c1d6135f5b6
Christian Cuevas <ccpotential@gmail.com>
2018-05-26 21:30:54 -0700

    recreating site
    Clark was here :V

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="icon" href="./assets/potential.svg" />
+		<title>Potential - Home</title>
+
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="description" content="Potential, unlock that which is unknown!" />
+		<meta name="keywords" content="www,potential" />
+
+		<!-- CSS -->
+		<style></style>
+		<link rel="stylesheet" type="text/css" href="./css/libs/semantic.min.css">
+	</head>
+	<body>
+
+		<!-- JS -->
+		<script></script>
+		<script src="./js/libs/jquery-3.3.1.min.js"></script>
+		<script src="./js/libs/semantic.min.js"></script>
+	</body>
+</html>
