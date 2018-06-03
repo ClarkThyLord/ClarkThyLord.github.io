b3dc8d0e101d8628f74a0fb2aec5808d8d13f037
Christian Cuevas <ccpotential@gmail.com>
2018-06-02 23:24:28 -0700

    better slide
    Clark was here :V

--- src/php/home.php
+++ src/php/home.php
@@ -1,3 +1,5 @@
-<div data-role="carousel">
+<div data-role="carousel" data-auto-start="true" data-bullet-style="circle" data-effect="fade" data-period="3000" data-duration="500">
 	<div class="slide" data-cover="./assets/art/Eye%20Of%20Potential.jpg"></div>
+	<div class="slide" data-cover="./assets/art/Kingdom%20Hearts%20Sky.jpg"></div>
+	<div class="slide" data-cover="./assets/design/Pierce%20The%20Veil.jpg"></div>
 </div>
