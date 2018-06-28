da29982a676b9f0dc70bdf18336469a78f421279
Christian Cuevas <ccpotential@gmail.com>
2018-06-27 20:51:24 -0700

    setup base server API
    Clark was here :V

---
+++ src/server/endpoints/projects.php
@@ -0,0 +1,12 @@
+<?php
+
+	/**
+	* Returns/Dumps a Array containing valid projects(s) found, with the given filter and options.
+	* @param filter [Array] Properties to get with.
+	* @param options [Array] Options to get with.
+	* @return {Array} Returns a Array containing valid projects(s) found.
+	*/
+	function projects_get($filter=array(), $options=array()) {
+	}
+
+?>
---
+++ src/server/endpoints/works.php
@@ -0,0 +1,12 @@
+<?php
+
+	/**
+	* Returns/Dumps a Array containing valid work(s) found, with the given filter and options.
+	* @param filter [Array] Properties to get with.
+	* @param options [Array] Options to get with.
+	* @return {Array} Returns a Array containing valid work(s) found.
+	*/
+	function works_get($filter=array(), $options=array()) {
+	}
+
+?>
