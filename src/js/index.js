1b2bafb50249eef58d446ea7d5a0adafdb9f2ce9
Christian Cuevas <ccpotential@gmail.com>
2018-07-07 17:59:04 -0700

    compress images and fix up
    Clark was here :V

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,10 @@ GLOBALS.projects = [];
 
 $(function() {
   $.getJSON('http://www-potential/server/api.php/works?filter={"type": "images"}&options={"max":5,"sort":"DES"}', function(response) {
-    console.log(response);
-    GLOBALS.works = response.data.works;
+    if (response.success) {
+      GLOBALS.works = response.data.works;
+    } else {
+      alert(response.reason);
+    }
   });
 });
